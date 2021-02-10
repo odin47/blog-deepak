d1837b8b1ba4b84a2e67fe1b46c6f8376609a5b2
deepak.devendra <deepak.devendra@lge.com>
2021-02-10 07:38:41 +0530

    Created About page
    1.) Created About page
    2.) Used css clamp function for overall fontsize
    3.) Added ArticleList component in blog page

---
+++ pages/about.js
@@ -0,0 +1,22 @@
+import Layout from 'components/Layout';
+import Avatar from 'components/Avatar';
+
+const About = () => {
+    return (
+        <Layout>
+            <div className={'mainContainer'}>
+                <div className={'vcenter'}>
+                    <h1>I am Deepak Podili Devendra</h1>
+                </div>
+                <div className={'topMargin3'}>
+                    <Avatar />
+                </div>
+                <p className={'topMargin3'}>
+                    Hello my name is Deepak Podili Devendra from Bangalore, India, I am a Full-stack developer with keen interest in solving real world problems. I have a diverse set of skills ranging from HTML, CSS, Javascript, ReactJS all the way to NodeJS, MYSQL. I like to stay updated on the latest trends in the IT industry.
+                </p>
+            </div>
+        </Layout>
+    )
+}
+
+export default About;
--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,14 @@
 import Layout from 'components/Layout';
 import TagList from 'components/TagList';
-import {getTagsList} from 'lib/posts';
+import ArticleList from 'components/ArticleList';
+import {getTagsList, getSortedPosts} from 'lib/posts';
 
-export default function Blog({allTagsList}) {
+export default function Blog({allTagsList, allPostsData}) {
     return (
         <Layout>
             <div className={'mainContainer'}>
                 <TagList list={allTagsList}/>
+                <ArticleList list={allPostsData}/>
             </div>
         </Layout>
     )
@@ -14,9 +16,11 @@ export default function Blog({allTagsList}) {
 
 export async function getStaticProps() {
     const allTagsList = getTagsList();
+	const allPostsData = getSortedPosts();
     return {
 		props: {
 			allTagsList,
+            allPostsData
 		},
 	};
 }
